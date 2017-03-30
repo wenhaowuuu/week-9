6003e7ac645fc6ddb3755d9116d03e50ed398b7c
wenhaowuuu <wenhaowu92@gmail.com>
2017-03-30 01:19:03 -0400

    Week#9

--- lab/lab2/js/main.js
+++ lab/lab2/js/main.js
@@ -63,7 +63,132 @@ Questions you should ask yourself:
   - What can I do with the output?
   - Can I get a lat/lng from the output?
 
+/* =====================
 
+=====================  */
+
+// https://valhalla.mapzen.com/route?api_key=mapzen-Dok7vcm&json={%22locations%22:[{%22lat%22:41.648188,%22lon%22:-86.133938},{%22lat%22:41.648188,%22lon%22:-86.1347758}],%22costing%22:%22auto%22,%22directions_options%22:{%22units%22:%22miles%22}}
+
+
+var url = "https://search.mapzen.com/v1/search?api_key=mapzen-Dok7vcm&text=";
+var newurl = "";
+
+var urlsmproute = "https://valhalla.mapzen.com/optimized_route?api_key=mapzen-Dok7vcm&json=";
+var newurlsmproute = "";
+
+var urlmultiroute = "https://valhalla.mapzen.com/route?api_key=mapzen-Dok7vcm&json=";
+var newurlmultiroute = "";
+
+var featureGroup = [];
+var filteredfeatureGroup = [];
+
+var jsontoadd = {"locations":[],"costing":"auto","directions_options":{"units":"miles"}};
+
+
+var OriginLat = "";
+var OriginLon = "";
+var DestLat = "";
+var DestLon = "";
+
+// TESTING WITH SIMPLE POINTS, BUT STILL GETTING ERRORS.
+// var routetest = "https://valhalla.mapzen.com/optimized_route?api_key=mapzen-Dok7vcm&json=";
+// var newroutetest = "";
+// var jsontoadd0 = {"locations":[{"lat":39.91,"lon":-73.16},{"lat":39.92,"lon":-73.15}],"costing":"auto","directions_options":{"units":"miles"}};
+// // {"locations":[{"lat":40.042072,"lon":-76.306572},{"lat":39.992115,"lon":-76.781559},{"lat":39.984519,"lon":-76.6956}],"costing":"auto","directions_options":{"units":"miles"}}
+// $(document).ready(function(){
+//   newroutetest = routetest + JSON.stringify(jsontoadd0);
+//   console.log("test1");
+//   console.log(newroutetest);
+//   $.ajax(newroutetest).done(function(data){
+//     console.log("test2");
+//     var parsedDatatest = JSON.parse(JSON.stringify(data));
+//     console.log(newroutetest);
+//   });
+// });
+
+
+$('#Search').click(function(){
+  $(document).ready(function() {
+    newurl = url + $('#dest').val();
+    console.log(newurl);
+    $.ajax(newurl).done(function(data){
+      console.log("downloaded");
+      var parsedData = JSON.parse(JSON.stringify(data));
+      console.log("parsed");
+      featureGroup.push(L.geoJSON(parsedData,{
+      }).addTo(map));
+      // DestLat = data.features[0].geometry.coordinates[1];
+      // DestLon = data.features[0].geometry.coordinates[0];
+      // console.log(featureGroup._layers[50]._latlng);
+    });
+  });
+});
+
+
+// Problem shooting:
+// Use JSON validator to test the problems.
+// use valhalla instead of matrix
+// limit it to the head one! you can use the underscore function "take"
+// var hundered =
+// if wanna do multiple points: just use route not optimized_route
+
+
+///////QUESTIONS 2:
+///////STILL NEED TO ASK??? HOW TO EXTRACT FROM OBJECT?// jsontoadd.locations.push({"lat":state.position.marker._latlng.lat,"lon":state.position.marker._latlng.lng},{"lat":featureGroup[0]._layers.100._latlng.lat,"lon":featureGroup[0]._layers.100._latlng.lng});
+
+//https://valhalla.mapzen.com/route?api_key=mapzen-Dok7vcm&json={%22locations%22:[{%22lat%22:41.648188,%22lon%22:-86.133938},{%22lat%22:40.369055,%22lon%22:-79.627758},{%22lat%22:39.791889,%22lon%22:-76.952834},{%22lat%22:40.178526,%22lon%22:-76.360958},{%22lat%22:39.822383,%22lon%22:-75.873631},{%22lat%22:40.780339,%22lon%22:-79.921847},{%22lat%22:40.817744,%22lon%22:-76.902074},{%22lat%22:40.371055,%22lon%22:-77.057653},{%22lat%22:40.102854,%22lon%22:-77.339839},{%22lat%22:40.436952,%22lon%22:-76.077936}],%22costing%22:%22auto%22,%22directions_options%22:{%22units%22:%22miles%22}}
+
+// "lat":state.position.marker._latlng.lat,"lon":state.position.marker._latlng.lon
+
+
+$('#SmpRoute').click(function(){
+  $(document).ready(function() {
+    jsontoadd.locations.push({"lat":OriginLat,"lon":OriginLon},{"lat":DestLat,"lon":DestLon});
+      console.log("points added");
+    // need to remove the last three points!!!
+    newurlsmproute = urlsmproute + JSON.stringify(jsontoadd);
+    // at=39.993096&focus.point.lon=-75.175034&boundary.circle.lat=39.993096&boundary.circle.lon=-75.175034&boundary.circle.radius=100';
+    console.log(newurlsmproute);
+    $.ajax(newurlsmproute).done(function(data){
+
+/////QUESTIONS 3:
+/////{"error_code":106,"error":"Try any of:'\/locate' '\/route' '\/trace_route' '\/trace_attributes' ","status_code":404,"status":"Not Found"}///////////////
+/////Or this one: // {"error_code":154,"error":"Path distance exceeds the max distance limit","status_code":400,"status":"Bad Request"}
+      console.log("downloaded1");
+
+    });
+  });
+});
+
+
+
+
+$('#Route').click(function(){
+  $(document).ready(function() {
+    // console.log(featureGroup[0]._layers[49]._latlng);
+    _.each(featureGroup[0]._layers,function(item){
+      jsontoadd.locations.push({"lat":item._latlng.lat,"lon":item._latlng.lng});
+      console.log("item run");
+    });
+
+    // need to remove the last three points!!!
+    newurlmultiroute = urlmultiroute + JSON.stringify(jsontoadd);
+    // at=39.993096&focus.point.lon=-75.175034&boundary.circle.lat=39.993096&boundary.circle.lon=-75.175034&boundary.circle.radius=100';
+    console.log(newurlmultiroute);
+
+    $.ajax(newurlmultiroute).done(function(data){
+      // {"error_code":154,"error":"Path distance exceeds the max distance limit","status_code":400,"status":"Bad Request"}
+      console.log("downloaded1");
+      var parsedPoints = JSON.parse(JSON.stringify(data));
+      filteredfeatureGroup.push(L.geoJSON(parsedPoints,{
+      }).addTo(map));
+      console.log("parsed1");
+    });
+  });
+});
+
+
+/* =====================
 Task 2: Use Mapzen's 'Mobility' API to generate a route based on your origin and destination
 
 The docs: https://mapzen.com/documentation/mobility/
@@ -72,6 +197,58 @@ option available: 'Optimized Route' (https://mapzen.com/documentation/mobility/o
 Once you're getting a valid (as best you can tell) response from the server, move
 to the next task.
 
+/* =====================
+
+=====================  */
+
+// var points = {"locations":[{"lat":,"lon":},{}]};
+
+
+$('#Decode').click(function(){
+  $(document).ready(function(){
+    $.ajax(newurlmultiroute).done(function(data){
+      var string = data.trip.legs[0].shape;
+      console.log("string");
+      var decodedData = decode(string,6);
+      console.log(decodedData);
+
+      //CONVERT TO GEOJSON LINE//
+      var PlotLine =
+      {
+              "id": "route",
+              "type": "line",
+              "source": {
+                  "type": "geojson",
+                  "data": {
+                      "type": "Feature",
+                      "properties": {},
+                      "geometry": {
+                          "type": "LineString",
+                          "coordinates": [
+                              [decodedData[0][1],decodedData[0][0]],
+                              [decodedData[1][1],decodedData[1][0]]
+                          ]
+                      }
+                  }
+              }
+      };
+      console.log(PlotLine);
+
+      /////////////QUESTIONS4:
+      /////////////always reporting "invalid geoJSON object".
+      // var newlayer = PlotLine.addTo(map);
+      L.geoJSON(PlotLine,{}).addTo(map);
+
+
+    });
+  });
+});
+
+// if (i<decodedData.length){
+//
+// };
+
+/* =====================
 
 Task 3: Decode Mapzen's route response
 
@@ -116,6 +293,10 @@ Task 6: (stretch) See if you can refocus the map to roughly the bounding box of 
 
 ===================== */
 
+//Mapzen tools
+//Mapzen Keys
+// optimized route
+
 var state = {
   position: {
     marker: null,
@@ -130,7 +311,6 @@ var goToOrigin = _.once(function(lat, lng) {
   map.flyTo([lat, lng], 14);
 });
 
-
 /* Given a lat and a long, we should create a marker, store it
  *  somewhere, and add it to the map
  */
@@ -142,11 +322,17 @@ var updatePosition = function(lat, lng, updated) {
   goToOrigin(lat, lng);
 };
 
+
+// var currentlocation = [lat,lng];
+
 $(document).ready(function() {
   /* This 'if' check allows us to safely ask for the user's current position */
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
       updatePosition(position.coords.latitude, position.coords.longitude, position.timestamp);
+      console.log("origin");
+      OriginLat = state.position.marker._latlng.lat;
+      OriginLon = state.position.marker._latlng.lng;
     });
   } else {
     alert("Unable to access geolocation API!");
@@ -171,5 +357,3 @@ $(document).ready(function() {
   });
 
 });
-
-
